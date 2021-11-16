7ada53b91d114114d3f34e933546ff77297bf443
Wolf Elkan <wolf.elkan@gmail.com>
2021-11-16 17:45:22 -0500

    Negative grid started

--- Maze.java
+++ Maze.java
@@ -1,5 +1,8 @@
 class Maze {
+	BoxChar b = new BoxChar();
 	private Node[][] nodes;
+	Node start = new Node();
+	Node end = new Node();
 	private int[] dirR = {-1, 0, 1, 0};
 	private int[] dirC = { 0, 1, 0,-1};
 	int nRows;
@@ -10,7 +13,12 @@ class Maze {
 		nodes = new Node[nRows][nCols];
 		for (int r=0; r<nRows; r++) {
 			for (int c=0; c<nCols; c++) {
-				nodes[r][c] = new Node();
+				nodes[r][c] = new Node(
+					r == 0,
+					c == nCols-1,
+					r == nRows-1,
+					c == 0
+				);
 				if (r > 0 && Math.random() < connectionProbability) {
 					connect(r, c, 0);
 				}
@@ -19,6 +27,8 @@ class Maze {
 				}
 			}
 		}
+		nodes[0][0].connect(start,0);
+		nodes[nRows-1][nCols-1].connect(end,2);
 	}
 	void connect(int row, int col, int dir) {
 		Node nodeA = nodes[row][col];
@@ -31,13 +41,63 @@ class Maze {
 		for (int r=0; r<nRows; r++) {
 			for (int c=0; c<nCols; c++) {
 				System.out.print(nodes[r][c]);
+				System.out.print(nodes[r][c].right());
 			}
 			System.out.println("");
 		}		
 	}
+	String pnWall(int r, int c) {
+		String result = "";
+		// int A = (r == 0 || c == 0) ? 0 : nodes[r-1][c].n(3) ? 0 : 2;
+		// int A;
+		System.out.print(nodes[r][c].wall());
+		// if (r == 0 || c == 0) {
+		// 	A = 0;
+		// } else {
+			// A = nodes[r][c].N2(3);
+		// }
+		// result += b.c(0,0,0,0);
+		// result += b.c(0,0,0,0);
+		// result += b.c(0,0,0,0);
+		// result += b.c(0,0,0,0);
+		return result;
+	}
+	String pnHall(int r, int c) {
+		String result = "";
+		System.out.print(nodes[r][c].hall());
+		// result += b.c(0,0,0,0);
+		// result += b.c(0,0,0,0);
+		// result += b.c(0,0,0,0);
+		// result += b.c(0,0,0,0);
+		return result;
+	}
+	String pnFloor(int r, int c) {
+		String result = "";
+		System.out.print(nodes[r][c].floor());
+		return result;
+	}
+	void printNeg() {
+		for (int r=0; r<nRows; r++) {
+			for (int c=0; c<nCols; c++) {
+				System.out.print(pnWall(r, c));
+			}
+			System.out.println("");
+			if (r < nRows) {
+				for (int c=0; c<nCols; c++) {
+					System.out.print(pnHall(r, c));
+				}
+				System.out.println("");
+			}
+		}	
+		for (int c=0; c<nCols; c++) {
+			System.out.print(pnFloor(nRows-1, c));
+		}
+		System.out.println("");
+	}
 	public static void main(String[] args) {
 		Maze maze = new Maze(8,8,0.5);
 		maze.connect(4,5,2);
-		maze.printMap();
+		// maze.printMap();
+		maze.printNeg();
 	}
 }
--- Node.java
+++ Node.java
@@ -1,9 +1,27 @@
 class Node {
-	BoxChar bp = new BoxChar();
+	BoxChar b = new BoxChar();
 	private Node[] neighbors = new Node[4];
+	private boolean[] edgecases = new boolean[4];
+	Node(int row, int col) {}
+	Node(boolean edge0, boolean edge1, boolean edge2, boolean edge3) {
+		edgecases[0] = edge0;
+		edgecases[1] = edge1;
+		edgecases[2] = edge2;
+		edgecases[3] = edge3;
+	}
+	Node () {}
 	void connect(Node target, int direction) {
 		neighbors[direction] = target;
 		target.neighbors[direction^2] = this;
+	}
+	boolean n(int dir) {
+		return neighbors[dir] != null;
+	}
+	int N(int dir) {
+		return n(dir) ? 1 : 0;
+	}
+	int N2(int dir) {
+		return n(dir) ? 0 : 2;
 	}
 	public String toString() {
 		String result = "";
@@ -11,21 +29,60 @@ class Node {
 			neighbors[1] == null &&
 			neighbors[2] == null &&
 			neighbors[3] == null) {
-			result += bp.dot();
+			result += b.dot();
 		} else {
-			result += bp.c(
-				neighbors[0] != null ? 1 : 0,
-				neighbors[1] != null ? 1 : 0,
-				neighbors[2] != null ? 1 : 0,
-				neighbors[3] != null ? 1 : 0
-			);
+			result += b.c(N(0),N(1),N(2),N(3));
 		}
-		result += bp.c(
-			0,
-			neighbors[1] != null ? 1 : 0,
-			0,
-			neighbors[1] != null ? 1 : 0
-		);
+		return result;
+	}
+	// public char left() {
+	// 	int h = N(3);
+	// 	return b.c(0,h,0,h);
+	// }
+	public char right() {
+		int h = N(1);
+		return b.c(0,h,0,h);
+	}
+	private int u() {
+		if (neighbors[0] == null) {
+			return 2;
+		} else {
+			return 0;
+		}
+	}
+	public String wall() {
+		int U = u();
+		String result = "";
+		result += b.c(2,2,2,2);
+		result += b.c(0,2,0,2);
+		result += b.c(0,2,0,2);
+		result += b.c(0,2,0,2);
+		if (edgecases[1]) {
+			result += b.c(2,0,2,2);
+		}
+		return result;
+	}
+	public String hall() {
+		String result = "";
+		result += b.c(2,0,2,0);
+		result += b.c(0,0,0,0);
+		result += b.c(N(0),N(1),N(2),N(3));
+		// result += b.c(0,0,0,0);
+		result += b.c(0,0,0,0);
+		if (edgecases[1]) {
+			result += b.c(2,0,2,0);
+		}
+		return result;
+	}
+	public String floor() {
+		String result = "";
+		result += b.c(2,2,0,2);
+		result += b.c(0,2,0,2);
+		result += b.c(0,2,0,2);
+		result += b.c(0,2,0,2);
+		if (edgecases[1]) {
+			result += b.c(2,0,0,2);
+		}
 		return result;
 	}
 	public static void main(String[] args) {
